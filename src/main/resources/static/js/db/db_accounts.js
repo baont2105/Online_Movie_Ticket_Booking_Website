90a0927c4f05bf485e35429576984ee9716ef8a6
baovirus <thaibaoo2105@gmail.com>
2025-03-01 23:39:00 +0700

    Tạo lại file Set Up
    tạo lại dự án mới

---
+++ src/main/resources/static/js/db/db_accounts.js
@@ -0,0 +1,23 @@
+var list_accounts = [
+  {
+    id: "0001",
+    email: "admin@gmail.com",
+    password: "123",
+    username: "admin",
+    photo: "avartar.png",
+  },
+  {
+    id: "0002",
+    email: "user@gmail.com",
+    password: "123",
+    username: "mèo nhếch mép",
+    photo: "cat.png",
+  },
+  {
+    id: "0003",
+    email: "admin@example.com",
+    password: "123",
+    username: "mèo Etou",
+    photo: "etou.png",
+  },
+];
