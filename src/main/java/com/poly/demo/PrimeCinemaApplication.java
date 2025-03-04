a59766affcab3446ef653206b92fcbac2b92df77
baovirus <thaibaoo2105@gmail.com>
2025-03-04 23:08:04 +0700

    sửa lỗi
    1

---
+++ src/main/java/com/poly/demo/PrimeCinemaApplication.java
@@ -0,0 +1,13 @@
+package com.poly.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeCinemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeCinemaApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/poly/demo/controllers/testController.java
@@ -0,0 +1,68 @@
+package com.poly.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class testController {
+	@RequestMapping("/")
+	public String index() {
+		return "index";
+	}
+
+	@RequestMapping("/ticketing")
+	public String ticketing() {
+		return "ticketing";
+	}
+
+	@RequestMapping("/promotion")
+	public String promotion() {
+		return "promotion";
+	}
+
+	@RequestMapping("/movies-list")
+	public String moviesList() {
+		return "movies-list";
+	}
+
+	@RequestMapping("/movie-detail")
+	public String movieDetail() {
+		return "movie-detail";
+	}
+
+	@RequestMapping("/home")
+	public String home() {
+		return "home";
+	}
+
+	@RequestMapping("/feedback")
+	public String feedback() {
+		return "feedback";
+	}
+
+	@RequestMapping("/account-information")
+	public String accountInformation() {
+		return "account-information";
+	}
+
+	@RequestMapping("/forgot-password")
+	public String forgotPassword() {
+		return "forgot-pass";
+	}
+
+	@RequestMapping("/login")
+	public String login() {
+		return "login";
+	}
+
+	@RequestMapping("/register")
+	public String register() {
+		return "register";
+	}
+
+	@RequestMapping("/my-tickets")
+	public String myTickets() {
+		return "my-tickets";
+	}
+}
