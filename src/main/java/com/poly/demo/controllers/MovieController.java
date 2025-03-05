37488c4932e48a9356b818ac88f7a3d4a9ce7a0e
baovirus <thaibaoo2105@gmail.com>
2025-03-06 00:45:51 +0700

    Code trang chi tiết phim
    code giao diện trang chi tiết phim

--- src/main/java/com/poly/demo/controllers/MovieController.java
+++ src/main/java/com/poly/demo/controllers/MovieController.java
@@ -1,11 +1,13 @@
 package com.poly.demo.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.poly.demo.entity.Movie;
@@ -13,16 +15,22 @@ import com.poly.demo.service.MovieService;
 
 
 @Controller
-@RequestMapping("/movies")
 public class MovieController {
 
     @Autowired
     private MovieService movieService;
 
-    @GetMapping
+    @RequestMapping("/movies")
     public String listMovies(Model model) {
         List<Movie> movies = movieService.getAllMovies();
         model.addAttribute("movies", movies);
         return "movies-list"; // Tên file Thymeleaf (movies.html)
+    }
+    
+    @RequestMapping("/movie-detail/{id}")
+    public String MovieDetail(@PathVariable Long id, Model model) {
+    	Optional<Movie> movie = movieService.getMovieById(id);
+    	model.addAttribute("movie", movie.get());
+    	return "movie-detail";
     }
 }
