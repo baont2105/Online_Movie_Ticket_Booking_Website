f11f5119cd112348b6ac94241111dc3584c66d83
baovirus <thaibaoo2105@gmail.com>
2025-03-08 00:01:13 +0700

    tối ưu đường dẫn ảnh
    a

--- src/main/java/com/poly/demo/controllers/AdminController.java
+++ src/main/java/com/poly/demo/controllers/AdminController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class AdminController {
 	@GetMapping("/")
 	public String AdminPage() {
-		return ("account-manager");
+		return ("dashboard");
 	}
 	
 }
