d561a043e8719152e98d322a7c64f069996cec65
baovirus <thaibaoo2105@gmail.com>
2025-03-09 00:45:39 +0700

    set up trang admin lần 1
    thêm giao diện trang admin

--- src/main/java/com/poly/demo/controllers/AdminController.java
+++ src/main/java/com/poly/demo/controllers/AdminController.java
@@ -1,6 +1,9 @@
 package com.poly.demo.controllers;
 
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,8 +11,59 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/admin")
 public class AdminController {
 	@GetMapping("/")
-	public String AdminPage() {
+	public String AdminPage(Model model) {
+		
+		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+		if (principal instanceof UserDetails) {
+			UserDetails user = (UserDetails) principal;
+			model.addAttribute("user", user); // Gửi user đến Thymeleaf
+		} else {
+			model.addAttribute("user", null); // Nếu chưa đăng nhập, user sẽ là null
+		}
 		return ("dashboard");
 	}
 	
+	@GetMapping("/accounts-manager")
+	public String AccountsManager(Model model) {
+		
+		//Xác định user hiện tại
+		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+		if (principal instanceof UserDetails) {
+			UserDetails user = (UserDetails) principal;
+			model.addAttribute("user", user); // Gửi user đến Thymeleaf
+		} else {
+			model.addAttribute("user", null); // Nếu chưa đăng nhập, user sẽ là null
+		}
+		return ("accounts-manager");
+	}
+	
+	@GetMapping("/movies-manager")
+	public String MoviesManager(Model model) {
+		
+		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+		if (principal instanceof UserDetails) {
+			UserDetails user = (UserDetails) principal;
+			model.addAttribute("user", user); // Gửi user đến Thymeleaf
+		} else {
+			model.addAttribute("user", null); // Nếu chưa đăng nhập, user sẽ là null
+		}
+		return ("movies-manager");
+	}
+	
+	@GetMapping("/tickets-manager")
+	public String TicketsManager(Model model) {
+		
+		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+		if (principal instanceof UserDetails) {
+			UserDetails user = (UserDetails) principal;
+			model.addAttribute("user", user); // Gửi user đến Thymeleaf
+		} else {
+			model.addAttribute("user", null); // Nếu chưa đăng nhập, user sẽ là null
+		}
+		return ("tickets-manager");
+	}
 }
